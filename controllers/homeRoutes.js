aab3617b2c7fde8b04b4bca5dc610cedfa4e450f
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-02-26 14:30:40 -0600

    merge from pull

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 
+const { Patient, Nutritionist } = require("../models");
+
 router.get("/", (req, res) => {
   res.render("homepage");
 });
 
 router.get("/admin", (req, res) => {
   res.render("adminpage");
+});
+
+router.get("/client", (req, res) => {
+  res.render("adminsview");
 });
 
 module.exports = router;
