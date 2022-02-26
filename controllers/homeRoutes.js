6818c4d04a2c86632fdc23f73fa6fb62b02b68a7
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-02-25 20:52:45 -0600

    admin handlebars modified and other files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.render('homepage');
-})
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
+
+router.get("/admin", (req, res) => {
+  res.render("adminpage");
+});
 
 module.exports = router;
