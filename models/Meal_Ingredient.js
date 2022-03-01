6c9d90919a2773ee15e91b7fffc54bd4feb4fbe0
abi <abimael.monarrez58@gmail.com>
2022-02-28 22:27:25 -0700

    create Meal_Ingredient model

---
+++ models/Meal_Ingredient.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Meal_Ingredient extends Model {}
+
+Meal_Ingredient.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        meal_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'meal',
+                key: 'id',
+            },
+        },
+
+        ingredient_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'ingredient',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'meal_ingredient',
+    }
+);
+
+module.exports = Meal_Ingredient;
