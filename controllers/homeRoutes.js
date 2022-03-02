2479a0129755794db4bd372a14d702b6b2c0b98c
Guillermo Lazo <glazovg@gmail.com>
2022-03-02 14:47:49 -0600

    Route plan modified, new partial file created

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { Patient, Nutritionist, Meal_Plan, Day, Meal, Day_Meal, Ingredient, Meal_Ingredient } = require("../models");
+const { Patient, Nutritionist, MealPlan, Day, Meal, DayMeal, Ingredient, MealIngredient } = require("../models");
 
 router.get("/", (req, res) => {
   res.render("homepage");
@@ -11,24 +11,48 @@ router.get("/admin", (req, res) => {
 });
 
 router.get("/client/:id", async (req, res) => {
-  try{
+  try {
     const clientData = await Patient.findByPk(req.params.id, {
       attributes: {
         exclude: ["password"],
       },
     });
-    const client = clientData.get({plain: true});
+    const client = clientData.get({ plain: true });
     console.log(client);
     res.render("adminsview", client);
-  }catch(err) {
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 
 //TODO: change /client/plan/ for /client/plan/:id once db tables are all set.
-router.get('/client/plan/', async (req, res) => {
+router.get('/plan/:id', async (req, res) => {
   try {
-    //TODO: Get data by plan_id 
+    //TODO: Get data by plan_id
+    const planData = await MealPlan.findOne({
+      include: [
+        {
+          model: Day,
+          include: [{
+            model: Meal,
+            include: [{
+              model: Ingredient
+            }]
+          }]
+        }
+      ]
+    });
+
+    const plan = planData.get({ plain: true });
+
+    //DELETE CONSOLE LOGS
+    console.log(plan);
+    console.log(plan.days);
+    console.log(plan.days[0].meals);
+
+    console.log(plan.days[0].meals[0].ingredients);
+
+    // res.status(200).json(plan);
     res.render('nutritionplan');
   } catch (err) {
     res.status(500).json(err);
