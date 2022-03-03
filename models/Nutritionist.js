bc3a53655115139d3feffa86f76efafd1a4970dc
Enoc Rojas <rojiyoyo@gmail.com>
2022-03-02 23:13:19 -0600

    Added Login/Logout and SignUp functionality

--- models/Nutritionist.js
+++ models/Nutritionist.js
@@ -3,7 +3,9 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class Nutritionist extends Model {
-    checkPassword(){}
+    checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+    }
 }
 
 Nutritionist.init(
