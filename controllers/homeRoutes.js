50601d7037d01ced5a6caf8c5cf81bde2a73ddf0
ernest1589 <87840643+ernest1589@users.noreply.github.com>
2022-03-03 20:09:31 -0600

    fixed bugs for program to run

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 
-const { Patient, Nutritionist, Day, Meal, DayMeal, Ingredient, MealIngredient } = require("../models");
-const withAuth = require('../utils/auth');
-
+const {
+  Patient,
+  Nutritionist,
+  Day,
+  Meal,
+  DayMeal,
+  Ingredient,
+  MealIngredient,
+} = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", (req, res) => {
   res.render("homepage");
 });
 
-router.get("/admin", async (req, res) => {
+router.get("/admin/:id", withAuth, async (req, res) => {
   try {
     const patientsData = await Patient.findAll({
       attributes: {
@@ -25,18 +32,19 @@ router.get("/admin", async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+});
 
-router.get('/admin', withAuth, async (req, res) => {
+router.get("/admin", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     console.log(req.session.user_id);
     const userData = await Nutritionist.findByPk(req.session.user_id, {
-      attributes: { exclude: ['last_name', 'first_name'] },
+      attributes: { exclude: ["last_name", "first_name"] },
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('adminpage', {
+    res.render("adminpage", {
       ...user,
       logged_in: req.session.logged_in,
     });
@@ -62,22 +70,21 @@ router.get("/client/:id", async (req, res) => {
       ...client,
       logged_in: req.session.logged_in,
     });
-  }catch(err) {
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 
 //TODO: change /client/plan/ for /client/plan/:id once db tables are all set.
-router.get('/client/:id/plan/', async (req, res) => {
+router.get("/client/:id/plan/", async (req, res) => {
   try {
-    //TODO: Get data by plan_id 
-    res.render('nutritionplan', {
+    //TODO: Get data by plan_id
+    res.render("nutritionplan", {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
