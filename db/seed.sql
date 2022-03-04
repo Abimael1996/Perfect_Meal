350cf2fd16eb91cc1998607e403420062b124bb9
abi <abimael.monarrez58@gmail.com>
2022-03-03 19:25:57 -0700

    fix bug

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,6 @@
 INSERT INTO nutritionist (first_name, last_name, email, password) 
 VALUES ("Valeria", "Villaseñor", "valevilla@gmail.com", "valerita1234");
 
-<<<<<<< HEAD
 INSERT INTO patient (first_name, last_name, email, password, weight, height, sex, goal, age, activity, nutritionist_id) 
 VALUES ("Abimael", "Monarrez", "abimaelmona@gmail.com", "abi123455", "169", "6.0", "Male", "Gain", "25", "Moderate", "1");
 
@@ -10,13 +9,3 @@ VALUES ("Roman", "Beltran", "roman23@gmail.com", "roman123455", "190", "5.10", "
 
 INSERT INTO patient (first_name, last_name, email, password, weight, height, sex, goal, age, activity, nutritionist_id) 
 VALUES ("Julia", "Osuna", "julia@gmail.com", "julia1898i9", "143", "5.06", "Female", "Maintain", "19", 'Athlete', "1");
-=======
-INSERT INTO patient (first_name, last_name, email, password, weight, height, sex, goal, age, nutritionist_id) 
-VALUES ("Abimael", "Monarrez", "abimaelmona@gmail.com", "abi123455", "169", "6.0", "male", "gain", "25", "1");
-
-INSERT INTO patient (first_name, last_name, email, password, weight, height, sex, goal, age, nutritionist_id) 
-VALUES ("Roman", "Beltran", "roman23@gmail.com", "roman123455", "190", "5.10", "male", "lose", "23", "1");
-
-INSERT INTO patient (first_name, last_name, email, password, weight, height, sex, goal, age, nutritionist_id) 
-VALUES ("Julia", "Osuna", "julia@gmail.com", "julia1898i9", "143", "5.06", "female", "lose", "19", "1");
->>>>>>> origin/main
